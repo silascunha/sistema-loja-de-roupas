cce9871968d63e9b2d3e1d89f0e8853b8a8e0a45
Silas Fernandes da Cunha <silascunha09@hotmail.com>
2020-10-05 21:24:28 -0300

    Projeto criado com Maven

---
+++ src/main/java/br/sistemalojaroupas/application/App.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.sistemalojaroupas.application;
+
+/**
+ *
+ * @author silas
+ */
+public class App {
+    
+    public static void main(String[] args) {
+        
+    }
+}
