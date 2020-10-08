9c0f3541e56c3686cabc83992035829d3a663c71
Silas Fernandes da Cunha <silascunha09@hotmail.com>
2020-10-08 19:08:16 -0300

    Criado o produtoDao e implementada a classe Produto

--- src/main/java/br/sistemalojaroupas/application/App.java
+++ src/main/java/br/sistemalojaroupas/application/App.java
@@ -11,8 +11,16 @@ import br.sistemalojaroupas.db.DB;
 public class App {
     
     public static void main(String[] args) {
-        DB.iniciar();
+        int n4 = multiplicar(2, 3);
+        int n3 = multiplicar(1, 3);
         
-        DB.fechar();
+        System.out.println(n4);
+        System.out.println(n3);
+    }
+    
+    public static int multiplicar(int n1, int n2) {
+        int soma = n1 * n2;
+        
+        return soma;
     }
 }
